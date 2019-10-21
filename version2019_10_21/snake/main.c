92f125d31130cd1eac6643c843c8482ebc5484a6
Arthurdbt <54934391+Arthurdbt@users.noreply.github.com>
2019-10-21 11:51:14 +0200

    Add files via upload
    Gestion de collision des murs du labyrinthe 
    
    Objectif : coffre + le snake dans le lab

---
+++ version2019_10_21/snake/main.c
@@ -0,0 +1,470 @@
+/**
+ * \file main.c
+ * \brief Programme principal du niveau 0
+ * \author BONTE Emeline, DEBART Arthur
+ * \version 0.1
+ * \date 12 septembre 2019
+ */
+
+#include "sdl-light.h"
+
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 640
+
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 450
+
+/**
+ * \brief Taille d'un parchemin
+ */
+#define SIZEH 45
+
+/**
+ * \brief Taille du bonhomme (sprite)
+ */
+#define SIZEL 45
+
+/**
+ * \brief Pas de déplacement du sprite (bonhomme)
+ */
+
+#define MOVING_STEP 1
+
+/**
+ * \brief Bordure droite de la table
+ */
+#define RIGHT_LIMIT 540
+
+/**
+ * \brief Bordure gauche de la table
+ */
+#define LEFT_LIMIT 0
+
+/**
+ * \brief Bordure haute de la table
+ */
+#define TOP_LIMIT 0
+
+/**
+ * \brief Bordure basse de la table
+ */
+#define BOTTOM_LIMIT 540
+
+/**
+ * \brief Bordure basse de la table
+ */
+#define SIZE_MOITIER 16
+
+/**
+ * \brief Nombre maximum de corps 
+ */
+#define NB_CORPS 100
+
+
+
+
+
+
+
+struct pomme_s{
+	SDL_Surface* Pomme;
+	int px,py;
+};
+
+typedef struct pomme_s pomme_t;
+
+struct corps_s{
+	SDL_Surface* Corps;
+	double cx,cy;
+};
+
+typedef struct corps_s corps_t;
+
+
+
+
+/**
+ * \brief Représentation du monde du jeu
+ */
+
+struct world_s{
+  SDL_Surface* background;
+  SDL_Surface* Serpent;
+  SDL_Surface* Serpent1;
+  SDL_Surface* Serpent2;
+  SDL_Surface* Serpent3;
+  SDL_Surface* Serpent4;
+  pomme_t Pomme_rouge;
+  corps_t corps;
+  corps_t C[NB_CORPS];
+  int gameover;
+  double x,y;
+  double vx, vy;
+  int direct;
+  int nb_corps;
+    
+};
+
+typedef struct world_s world_t;
+
+
+/**
+ * \brief La fonction initialise les données du monde du jeu
+ * \param world les données du monde
+ */
+
+
+void init_data(world_t * world){ 
+  world-> background = load_image( "ressources/herbe.bmp" );
+  world-> Pomme_rouge.Pomme = load_image( "ressources/pomme.bmp" );
+  world-> corps.Corps = load_image("ressources/corps.bmp");
+  world-> gameover = 0;
+  world-> Serpent4 = load_image( "ressources/serpent_tete.bmp");
+  world-> Serpent3 = load_image( "ressources/serpent_tete_4.bmp");
+
+  world-> Serpent2 = load_image( "ressources/serpent_tete_2.bmp");
+  world-> Serpent1 = load_image( "ressources/serpent_tete_3.bmp");
+  world-> Serpent = load_image( "ressources/serpent_tete.bmp");//Le premier serpent
+  world-> x = 0.;
+  world-> y = 0.;
+
+
+  world->C[0].cx = 0;
+  world->C[0].cy = 32;
+  world->C[0].Corps = world->corps.Corps;
+
+	for(int i = 1; i<NB_CORPS ; i++){
+		world->C[i].cx = world->C[i-1].cx;
+		world->C[i].cy = world->C[i-1].cy + 9;
+		world->C[i].Corps = world->corps.Corps;
+	}
+	
+	world-> vx = 0.3;
+	world-> vy = 0.3;
+	
+
+  world->direct = 1;
+  world->nb_corps = 27;
+}
+ 
+
+
+/**
+ * \brief La fonction met à jour les données du serpent
+ * \param world les données du monde
+ */
+void update_serp(world_t *world){
+	if(world->direct == 0 ){ //droite
+		world->x+= world->vx;
+		for(int i = 0; i<world->nb_corps ; i++){
+			world->C[i].cx += world->vx;
+		}
+	}
+	if(world->direct == 1){//bas
+		world->y+= world->vy;
+		for(int i = 0; i<world->nb_corps ; i++){
+			world->C[i].cy += world->vy;
+		}
+	}
+	if(world->direct == 2){//gauche
+		world->x-= world->vx;
+		for(int i = 0; i<world->nb_corps ; i++){
+			world->C[i].cx -= world->vx;
+		}
+	}
+	if(world->direct == 3){//haut
+		world->y-= world->vy;
+		for(int i = 0; i<world->nb_corps ; i++){
+			world->C[i].cy -= world->vy;
+		}
+	}
+  /*world->vx = world->vx *(1-FREIN_COEF);
+  world->vy = world->vy *(1-FREIN_COEF);*/
+
+  //int touch = touch_bordure_b(&world->boule_blanche);
+  //miroir_rebond(&world->boule_blanche, touch);
+}
+
+
+
+
+/**
+ * \brief La fonction nettoie les données du monde
+ * \param world les données du monde
+ */
+
+
+void clean_data(world_t *world){
+  SDL_FreeSurface(world->background);
+  SDL_FreeSurface(world->Pomme_rouge.Pomme);
+  SDL_FreeSurface(world->corps.Corps);
+  SDL_FreeSurface(world->Serpent);
+  SDL_FreeSurface(world->Serpent1);
+  SDL_FreeSurface(world->Serpent2);
+  SDL_FreeSurface(world->Serpent3);
+  SDL_FreeSurface(world->Serpent4);
+}
+
+
+
+
+/**
+ * \brief La fonction indique si le jeu est fini en fonction des données du monde
+ * \param world les données du monde
+ * \return 1 si le jeu est fini, 0 sinon
+ */
+
+int is_game_over(world_t *world){
+  return world->gameover;
+}
+
+
+/**
+ * \brief La fonction met à jour les données en tenant compte de la physique du monde
+ * \param les données du monde
+ */
+
+void update_data(world_t *world){
+	update_serp(world);
+}
+
+
+/**
+ * \brief La fonction initialise les transparences des différentes surfaces
+ * \param screen la surface correspondant à l'écran de jeu
+ * \param world les données du monde
+ */
+
+void  init_graphics(SDL_Surface *screen, world_t *world){
+  set_transparence(screen, world->Pomme_rouge.Pomme, 255,255,255);
+  set_transparence(screen, world->Serpent, 255,255,255);
+  set_transparence(screen, world->Serpent1, 255,255,255);
+  set_transparence(screen, world->Serpent2, 255,255,255);
+  set_transparence(screen, world->Serpent3, 255,255,255);
+  set_transparence(screen, world->Serpent4, 255,255,255);
+  set_transparence(screen, world->corps.Corps, 255,255,255);
+
+   
+}
+
+int collision (world_t *world){
+	if(world->x+32 > SCREEN_WIDTH){
+		//world->x = 0.1;
+		return 1;
+	}
+	if(world->x < 0.){
+		//world->x = SCREEN_WIDTH - 20;
+		return 2;
+	}
+	if(world->y+32 > SCREEN_HEIGHT){
+		//world->y = 0.1;
+		return 3;
+	}
+	if(world->y < 0.){
+		//world->y = SCREEN_HEIGHT - 20;
+		return 4;
+	}
+	return 0;
+}
+ 
+void replacement_serpent(world_t *world){
+	if(collision(world) == 1){
+		world->x = 0.1;
+		return;
+	}
+	if(collision(world) == 2){
+		world->x = SCREEN_WIDTH - 32;
+		return;
+	}
+	if(collision(world) == 3){
+		world->y = 0.1;
+		return;
+	}
+	if(collision(world) == 4){
+		world->y = SCREEN_HEIGHT - 32;
+		return;
+	}
+	return;
+}
+
+void collision_pomme_serp(world_t* world){
+}
+
+
+
+
+/**
+ * \brief La fonction applique la surface de l'image de fond à quatre positions différentes sur l'écran de jeu, de sorte de complètement couvrir ce dernier
+ * \param screen l'écran de jeu
+ * \param bg la surface de l'image de fond
+ */
+void apply_background(SDL_Surface *bg, SDL_Surface *screen){
+   apply_surface(bg,screen,0,0);
+
+	
+}
+
+
+
+
+/**
+ * \brief La fonction rafrachit l'écran en fonction de l'état des données du monde
+ * \param screen la surface de l'écran de jeu
+ * \param world les données du monde
+ */
+
+void refresh_graphics(SDL_Surface *screen, world_t *world){
+  apply_background(world->background,screen);
+  apply_surface(world->Pomme_rouge.Pomme, screen, 50, 50);
+  //apply_surface(world->corps.Corps, screen, world->corps.cx , world->corps.cy);
+  apply_surface(world->Serpent, screen, world->x , world->y);
+  for(int i = 0; i<world->nb_corps; i++){
+	apply_surface(world->C[i].Corps, screen, world->C[i].cx , world->C[i].cy);
+  }
+  
+  refresh_surface(screen);
+}
+
+/**
+ * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+ * \param event paramètre qui contient les événements
+ * \param world les données du monde
+ */
+
+
+void handle_events(SDL_Event *event,world_t *world){
+  Uint8 *keystates;
+  update_data(world);
+
+  while( SDL_PollEvent( event ) ) {
+    //Si l'utilisateur a cliqué sur le X de la fenêtre
+    if( event->type == SDL_QUIT ) {
+      //On quitte le programme
+      world->gameover = 1;
+    }
+        
+    /* gestion des evenements clavier */
+    keystates = SDL_GetKeyState( NULL );
+
+    /* Si l'utilisateur appuie sur les flèches, le personnage se déplace */
+    if( keystates[ SDLK_UP])
+      {
+	world->direct = 3;
+	world->Serpent = world->Serpent4;
+	world->y-=10;
+	world->C[0].cx = world->x;
+	world->C[0].cy = world->y + 32 ;
+	for(int i = 1; i<world->nb_corps ; i++){
+		world->C[i].cx = world->C[i-1].cx;
+		world->C[i].cy = world->C[i-1].cy + 9;
+		/*world->C[i].cx = world->x;
+		world->C[i].cy = world->y + 32 ;*/
+	}
+	/*world->corps.cx = world->x;
+	world->corps.cy = world->y + 32 ;*/
+
+      }
+    else if( keystates[ SDLK_DOWN])
+      {
+	world->direct = 1;
+	world->Serpent = world->Serpent1;
+	world->y+=8;
+	world->C[0].cx = world->x + 1;
+	world->C[0].cy = world->y - 32 + 23;
+	for(int i = 1; i<world->nb_corps ; i++){
+		world->C[i].cx = world->C[i-1].cx;
+		world->C[i].cy = world->C[i-1].cy - 9;
+		/*world->C[i].cx = world->x + 1;
+		world->C[i].cy = world->y - 32 + 23;*/
+	}
+	/*world->corps.cx = world->x + 1;
+	world->corps.cy = world->y - 32 + 23;*/
+	
+      }
+    else if( keystates[ SDLK_RIGHT])
+      {
+	world->direct = 0;
+	world->Serpent = world->Serpent2;
+	world->x+=8;
+	world->C[0].cx = world->x - 32 + 12 ;
+	world->C[0].cy = world->y + 11 ;
+	for(int i = 1; i<world->nb_corps ; i++){
+		world->C[i].cx = world->C[i-1].cx - 9;
+		world->C[i].cy = world->C[i-1].cy;
+		
+		/*world->C[i].cx = world->x - 32 + 12 ;
+		world->C[i].cy = world->y + 11 ;*/
+	}
+	/*world->corps.cx = world->x - 32 + 12 ;
+	world->corps.cy = world->y + 11 ;*/
+      }
+    else if( keystates[ SDLK_LEFT])
+      {
+	world->direct = 2;
+	world->Serpent = world->Serpent3;
+	world->x-=8;
+	world->C[0].cx = world->x + 32 - 11;
+	world->C[0].cy = world->y + 12;
+	for(int i = 1; i<world->nb_corps ; i++){
+		world->C[i].cx = world->C[i-1].cx + 9;
+		world->C[i].cy = world->C[i-1].cy;
+		
+		/*world->C[i].cx = world->x + 32 - 11;
+		world->C[i].cy = world->y + 12;*/
+	}
+	/*world->corps.cx = world->x + 32 - 11;
+	world->corps.cy = world->y + 12;*/
+      }
+        
+  }
+    
+}
+
+
+
+/**
+ *  \brief programme principal qui implémente la boucle du jeu
+ */
+
+
+int main( int argc, char* args[] )
+{
+	
+  SDL_Event event;
+  world_t world;
+  SDL_Surface *screen;
+
+  screen = init_sdl(SCREEN_WIDTH, SCREEN_HEIGHT);
+  SDL_WM_SetCaption("Fenêtre de jeu de labyrinthe !", NULL);
+    
+  init_data(&world);
+  init_graphics(screen,&world);
+  
+  SDL_EnableKeyRepeat(100, 100); //active la répétition des touches
+ 
+  
+  while(!is_game_over(&world)){
+    
+    handle_events(&event,&world);
+    update_data(&world);
+    refresh_graphics(screen,&world);
+    SDL_Delay(10);
+	/*if(touch_bordure_b(&world)!=0){
+		replacement_mur(&world);
+	}*/
+	if(collision(&world)){
+		replacement_serpent(&world);
+	}
+
+  }
+
+  clean_data(&world);
+  quit_sdl();
+    
+    
+  return 0;
+}
